90dd540d870d49fad31afacb88d30bafa562d5df
Ilyosbek <ilyosbekorolov135@gmail.com>
2026-09-04 09:51:28 +0500

    fix: api/index.ts bom

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-﻿export default async function handler(req: any, res: any) {
+export default async function handler(req: any, res: any) {
   try {
     const module = await import('../server');
     const app = module.default || module.app;
